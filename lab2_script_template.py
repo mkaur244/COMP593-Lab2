08a422b4c7b395e8a5ac3e8e192a0cdeaee0ee3b
mkaur244 <156551724+mkaur244@users.noreply.github.com>
2024-01-25 11:19:04 -0500

    step 5 done

--- lab2_script_template.py
+++ lab2_script_template.py
@@ -25,6 +25,9 @@ def print_student_name_and_id(about_me):
     
 # TODO: Step 5 - Function that adds pizza toppings to data structure
 def add_pizza_toppings(about_me, toppings):
+    add_pizza_toppings : ['cheese','onion']
+                      
+     
     return
 
 # TODO: Step 6 - Function that prints bullet list of pizza toppings
